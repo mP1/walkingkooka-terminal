0da7ac2413ee7580bcc0536aa8f1babc3f0dd66b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-08 09:15:19 +1100

    TerminalExpressionFunctionShell.apply dead code removed

--- src/main/java/walkingkooka/terminal/expression/function/TerminalExpressionFunctionShell.java
+++ src/main/java/walkingkooka/terminal/expression/function/TerminalExpressionFunctionShell.java
@@ -117,47 +117,45 @@ final class TerminalExpressionFunctionShell<C extends TerminalExpressionEvaluati
                             buffer.toString()
                         ).toString()
                     );
-                    //if (null != value) {
-                        try {
-                            final String outputString;
-                            final String errorString;
+                    try {
+                        final String outputString;
+                        final String errorString;
 
-                            // print SpreadsheetError#message which has detailed message
-                            // #NAME?
-                            if (value instanceof HasTerminalOutputText) {
-                                final HasTerminalOutputText hasTerminalOutputText = (HasTerminalOutputText) value;
-                                outputString = hasTerminalOutputText.terminalOutputText();
-                                errorString = null;
+                        // print SpreadsheetError#message which has detailed message
+                        // #NAME?
+                        if (value instanceof HasTerminalOutputText) {
+                            final HasTerminalOutputText hasTerminalOutputText = (HasTerminalOutputText) value;
+                            outputString = hasTerminalOutputText.terminalOutputText();
+                            errorString = null;
+                        } else {
+                            if (value instanceof HasTerminalErrorText) {
+                                outputString = null;
+
+                                final HasTerminalErrorText hasTerminalErrorText = (HasTerminalErrorText) value;
+                                errorString = hasTerminalErrorText.terminalErrorText();
+
                             } else {
-                                if (value instanceof HasTerminalErrorText) {
-                                    outputString = null;
+                                outputString = context.convertOrFail(
+                                    value,
+                                    String.class
+                                );
 
-                                    final HasTerminalErrorText hasTerminalErrorText = (HasTerminalErrorText) value;
-                                    errorString = hasTerminalErrorText.terminalErrorText();
-
-                                } else {
-                                    outputString = context.convertOrFail(
-                                        value,
-                                        String.class
-                                    );
-
-                                    errorString = null;
-                                }
+                                errorString = null;
                             }
-
-                            if (null != outputString) {
-                                output.println(outputString);
-                            }
-                            if (null != errorString) {
-                                error.println(errorString);
-                            }
-                        } catch (final UnsupportedOperationException rethrow) {
-                            throw rethrow;
-                        } catch (final RuntimeException cause) {
-                            error.println(cause.getMessage());
-                            error.flush();
                         }
-                    //}
+
+                        if (null != outputString) {
+                            output.println(outputString);
+                        }
+                        if (null != errorString) {
+                            error.println(errorString);
+                        }
+                    } catch (final UnsupportedOperationException rethrow) {
+                        throw rethrow;
+                    } catch (final RuntimeException cause) {
+                        error.println(cause.getMessage());
+                        error.flush();
+                    }
                 } finally {
                     output.flush(); // evaluated expression/function might have printed but not flushed.
                     error.flush();
