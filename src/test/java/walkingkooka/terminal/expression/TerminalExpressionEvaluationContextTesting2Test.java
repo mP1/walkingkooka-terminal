8811bc4cb976d1df8d886346e45675662bb5df54
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-08-21 21:16:05 +1000

    TerminalExpressionEvaluationContextTesting2Test XXXTesting constants replace expressions

--- src/test/java/walkingkooka/terminal/expression/TerminalExpressionEvaluationContextTesting2Test.java
+++ src/test/java/walkingkooka/terminal/expression/TerminalExpressionEvaluationContextTesting2Test.java
@@ -21,16 +21,12 @@ import walkingkooka.convert.BinaryNumberConverterFunctions;
 import walkingkooka.convert.ConverterContexts;
 import walkingkooka.convert.Converters;
 import walkingkooka.currency.CurrencyLocaleContexts;
-import walkingkooka.datetime.DateTimeContexts;
-import walkingkooka.datetime.DateTimeSymbols;
+import walkingkooka.datetime.DateTimeContextTesting;
 import walkingkooka.environment.EnvironmentContext;
 import walkingkooka.environment.EnvironmentContextDelegator;
-import walkingkooka.environment.EnvironmentContexts;
 import walkingkooka.environment.EnvironmentValueName;
-import walkingkooka.environment.FakeEnvironmentContext;
-import walkingkooka.locale.LocaleContexts;
 import walkingkooka.math.DecimalNumberContext;
-import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.math.DecimalNumberContextDelegator;
 import walkingkooka.storage.StorageEnvironmentContext;
 import walkingkooka.storage.StorageEnvironmentContexts;
 import walkingkooka.terminal.TerminalContext;
@@ -39,7 +35,6 @@ import walkingkooka.terminal.TerminalContexts;
 import walkingkooka.terminal.TerminalId;
 import walkingkooka.terminal.expression.TerminalExpressionEvaluationContextTesting2Test.TestTerminalExpressionEvaluationContext;
 import walkingkooka.text.CaseSensitivity;
-import walkingkooka.text.LineEnding;
 import walkingkooka.tree.expression.CanEvaluateString;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
 import walkingkooka.tree.expression.ExpressionEvaluationContextDelegator;
@@ -49,14 +44,15 @@ import walkingkooka.tree.expression.ExpressionReference;
 import walkingkooka.tree.expression.convert.ExpressionNumberConverterContexts;
 
 import java.math.MathContext;
-import java.text.DateFormatSymbols;
 import java.time.LocalDateTime;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class TerminalExpressionEvaluationContextTesting2Test implements TerminalExpressionEvaluationContextTesting2<TestTerminalExpressionEvaluationContext> {
+public class TerminalExpressionEvaluationContextTesting2Test implements TerminalExpressionEvaluationContextTesting2<TestTerminalExpressionEvaluationContext>,
+    DateTimeContextTesting,
+    DecimalNumberContextDelegator {
 
     @Override
     public void testSetEnvironmentContextWithEqualEnvironmentContext() {
@@ -73,76 +69,19 @@ public class TerminalExpressionEvaluationContextTesting2Test implements Terminal
         return new TestTerminalExpressionEvaluationContext();
     }
 
-    private final static DecimalNumberContext DECIMAL_NUMBER_CONTEXT = DecimalNumberContexts.american(MathContext.DECIMAL32);
-
     @Override
     public MathContext mathContext() {
-        return DECIMAL_NUMBER_CONTEXT.mathContext();
-    }
-
-    @Override
-    public String currencySymbol() {
-        return DECIMAL_NUMBER_CONTEXT.currencySymbol();
+        return MATH_CONTEXT;
     }
 
     @Override
     public int decimalNumberDigitCount() {
-        return DECIMAL_NUMBER_CONTEXT.decimalNumberDigitCount();
+        return DEFAULT_NUMBER_DIGIT_COUNT;
     }
 
     @Override
-    public char decimalSeparator() {
-        return DECIMAL_NUMBER_CONTEXT.decimalSeparator();
-    }
-
-    @Override
-    public String exponentSymbol() {
-        return DECIMAL_NUMBER_CONTEXT.exponentSymbol();
-    }
-
-    @Override
-    public char groupSeparator() {
-        return DECIMAL_NUMBER_CONTEXT.groupSeparator();
-    }
-
-    @Override
-    public String infinitySymbol() {
-        return DECIMAL_NUMBER_CONTEXT.infinitySymbol();
-    }
-
-    @Override
-    public char monetaryDecimalSeparator() {
-        return DECIMAL_NUMBER_CONTEXT.monetaryDecimalSeparator();
-    }
-
-    @Override
-    public String nanSymbol() {
-        return DECIMAL_NUMBER_CONTEXT.nanSymbol();
-    }
-
-    @Override
-    public char percentSymbol() {
-        return DECIMAL_NUMBER_CONTEXT.percentSymbol();
-    }
-
-    @Override
-    public char permillSymbol() {
-        return DECIMAL_NUMBER_CONTEXT.permillSymbol();
-    }
-
-    @Override
-    public char negativeSign() {
-        return DECIMAL_NUMBER_CONTEXT.negativeSign();
-    }
-
-    @Override
-    public char positiveSign() {
-        return DECIMAL_NUMBER_CONTEXT.positiveSign();
-    }
-
-    @Override
-    public char zeroDigit() {
-        return DECIMAL_NUMBER_CONTEXT.zeroDigit();
+    public DecimalNumberContext decimalNumberContext() {
+        return DECIMAL_NUMBER_CONTEXT;
     }
 
     @Override
@@ -273,10 +212,8 @@ public class TerminalExpressionEvaluationContextTesting2Test implements Terminal
 
         @Override
         public ExpressionEvaluationContext expressionEvaluationContext() {
-            final Locale locale = Locale.ENGLISH;
-
             return ExpressionEvaluationContexts.basic(
-                ExpressionNumberKind.BIG_DECIMAL,
+                EXPRESSION_NUMBER_KIND,
                 (e, c) -> {
                     Objects.requireNonNull(e, "expression");
                     throw new UnsupportedOperationException();
@@ -306,26 +243,13 @@ public class TerminalExpressionEvaluationContextTesting2Test implements Terminal
                         BinaryNumberConverterFunctions.fake(), // multiplier
                         BINARY_TEXT_CONTEXT,
                         CurrencyLocaleContexts.fake(),
-                        DateTimeContexts.basic(
-                            DateTimeSymbols.fromDateFormatSymbols(
-                                new DateFormatSymbols(locale)
-                            ),
-                            locale,
-                            1950, // defaultYear
-                            50, // twoDigitYear
-                            EnvironmentContexts.fake()
-                        ),
+                        DATE_TIME_CONTEXT,
                         DECIMAL_NUMBER_CONTEXT
                     ),
                     ExpressionNumberKind.DEFAULT
                 ),
-                new FakeEnvironmentContext() {
-                    @Override
-                    public LineEnding lineEnding() {
-                        return TerminalExpressionEvaluationContextDelegatorTest.LINE_ENDING;
-                    }
-                },
-                LocaleContexts.jre(locale)
+                STORAGE_ENVIRONMENT_CONTEXT.cloneEnvironment(),
+                LOCALE_CONTEXT
             );
         }
 
